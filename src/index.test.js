074811537ab5f1c9e627b4c62c2cb11800c89d41
Rikesh <rikesh.shrestha@citytech.global>
2019-09-10 16:26:13 +0545

    here working

---
+++ src/index.test.js
@@ -0,0 +1,15 @@
+import {sensitiveWords} from './index'
+
+test('replaces the sensitive string into ***',()=>(
+    expect(sensitiveWords(
+        'The name of the NX will be nitendo switch',
+        ['switch']
+    )).toBe('The name of the NX will be nitendo ***')
+))
+
+test('replaces the multiple sensitive string into ***',()=>(
+    expect(sensitiveWords(
+        'The name of the NX will be nitendo switch, switch is great. switch it up.',
+        ['switch']
+    )).toBe('The name of the NX will be nitendo ***, *** is great. *** it up.')
+))
